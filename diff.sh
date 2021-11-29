f632cde24623c7762706650835452189587130f9
Maxwell Talbot <talbot.maxwell@gmail.com>
2021-11-29 15:11:40 +0000

    finally figured it out.. right

--- diff.sh
+++ diff.sh
@@ -8,12 +8,8 @@ ACTIVE_BRANCH=${GITHUB_REF##*/}
 cd $WORKSPACE
 
 # https://stackoverflow.com/questions/58033366/how-to-get-the-current-branch-within-github-actions
-echo $(git status)
-echo $(git fetch && git rev-list --left-right --count origin/$TARGET_BRANCH...origin/$ACTIVE_BRANCH)
 DIFF_COUNT=$(git fetch && git rev-list --left-right --count origin/$TARGET_BRANCH...origin/$ACTIVE_BRANCH | awk '{print $2}')
 echo $DIFF_COUNT
-
-echo "Diff count is $DIFF_COUNT"
 
 if [ "$DIFF_COUNT" == "0" ]
 then 
@@ -21,11 +17,10 @@ then
 else 
     DIFF_URL=https://github.com/$NAME/compare/$TARGET_BRANCH...$ACTIVE_BRANCH
 
-    # NOTE: not adding this message at the moment. its pretty spammy. 
+    # NOTE: not using this message at the moment. its pretty spammy, leaving it here though 
     MESSAGE=$(git log --color --graph --pretty=format:'%h -%d %s (%cr) <%an>' --abbrev-commit  --left-right origin/$TARGET_BRANCH...origin/$ACTIVE_BRANCH)
-    echo $MESSAGE
 
     # Just full on assuming this is behind for the time being. 
     # Could put in some 'useful' links into this message. Once we have something useful to do (other than update & rebuild)
-    # curl -X POST -H 'Content-type: application/json' --data '{"text":" `'$NAME'`: `'$TARGET_BRANCH'` is behind `'$ACTIVE_BRANCH'` by <'$DIFF_URL'|'$DIFF_COUNT' commit(s)>."}' $SLACK_WEBHOOK
+    curl -X POST -H 'Content-type: application/json' --data '{"text":" `'$NAME'`: `'$TARGET_BRANCH'` is behind `'$ACTIVE_BRANCH'` by <'$DIFF_URL'|'$DIFF_COUNT' commit(s)>."}' $SLACK_WEBHOOK
 fi 
